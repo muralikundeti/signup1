36c169b2ed6a348e0b4df72389a3f0c5c9ff3e88
murali kundeti <muralikundeti@gmail.com>
2022-11-25 16:22:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import RegistrationForm from './components/RegistationForm'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
